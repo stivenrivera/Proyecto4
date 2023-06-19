5f5388b64455eef54130dcc36abb60a317a5eec7
Stiven Rivera <115115438+stivenrivera@users.noreply.github.com>
2023-06-19 11:04:51 -0500

    Update calculadora.js

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -1,18 +1,20 @@
 
 
-
 funcionPredeterminada = () => {
     const formulario = document.getElementById('formulario')
     formulario.addEventListener('submit', (evento) => {
         evento.preventDefault();
-        let valorporHora = document.getElementById('valorPorHora');
-        valorporHora.value()
-        let disenoInterfaz= document.getElementById('interfazHoras');
-        disenoInterfaz.value()
-        let HorasHTML= document.getElementById('interfazHTML');
-        HorasHTML.value()
-        document.getElementById('valorTotal') = (valorporHora*disenoInterfaz)/HorasHTML;
+        let valorporHora = document.getElementById('valorPorHora').value;
+        let disenoInterfaz= document.getElementById('interfazHoras').value;
+        let HTML = document.getElementById('htmlhoras').value
+        let dificultad = document.getElementById('htmldificult').value
+        let valor= valorporHora * disenoInterfaz * HTML;
+        document.getElementById('valorTotal').value = dificultad*valor
+        
+        
+        
         
       })
 };
 funcionPredeterminada();
+
